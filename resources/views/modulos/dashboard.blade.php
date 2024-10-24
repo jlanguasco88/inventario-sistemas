d0a1792e4018872956ed9a8ca484cf759ecae601
juanlanguasco88 <juanmarcelolanguasco@gmail.com>
2024-10-24 11:49:36 -0300

    se modifica logo y logo2

--- resources/views/modulos/dashboard.blade.php
+++ resources/views/modulos/dashboard.blade.php
@@ -5,8 +5,8 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="#"><img src="" alt="Logo"></a>
-                <a class="navbar-brand hidden" href="#"><img src="/admin/images/logo2.png" alt="Logo"></a>
+                <a class="navbar-brand" href="#"><img src="logo.png" alt="Logo" height="100%" width="100%"></a>
+                <a class="navbar-brand hidden" href="#"><img src="logo2.png" alt="Logo"></a>
             </div>
 
             <div id="main-menu" class="main-menu collapse navbar-collapse">
